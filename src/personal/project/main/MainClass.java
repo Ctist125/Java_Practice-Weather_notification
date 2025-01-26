185628155d297b407adf6252d9eea14a26bd5f85
Tief <tief125@naver.com>
2025-01-26 14:42:36 +0900

    JAVA 공부 1일차 실습 - println()과 주석

---
+++ src/personal/project/main/MainClass.java
@@ -0,0 +1,17 @@
+package personal.project.main;
+
+/** <pre>
+ * 날씨정보와 함께 옷차림을 추천해주는 프로젝트
+ * 메인 클래스
+ * </pre>
+ */
+public class MainClass {
+	
+	/** 메인 함수 */
+	public static void main(String[] args) {
+		
+		// 초반 가이드
+		System.out.println("해당 프로그램은 날씨 정보를 알려주는 프로그램입니다.");
+	}
+}
+
