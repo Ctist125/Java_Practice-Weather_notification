7003677896daae8995c9ad30ec59fbd82e564115
Tief <tief125@naver.com>
2025-02-03 21:28:22 +0900

    함수 형식으로 코드 정리

--- src/personal/project/main/MainClass.java
+++ src/personal/project/main/MainClass.java
@@ -1,9 +1,10 @@
 package personal.project.main;
 
 /** 외부 클래스 */
-import personal.project.main.vo.WeatherVo; // 날씨, 기온, 옷차림 등 정보를 가져오는 클래스
+import personal.project.main.vo.WeatherVo; // 날씨, 기온, 옷차림 등 정보와 관련된 클래스
 
-/** <pre>
+/**
+ * <pre>
  * 날씨정보와 함께 옷차림을 추천해주는 프로젝트
  * 메인 클래스
  * </pre>
@@ -21,30 +22,15 @@ public class MainClass {
 		WeatherVo tomorrow = new WeatherVo();
 		
 		/** 날짜별 속성 정보 입력 */
-		today.weather = "비";
-		today.highestTemperature = 4;
-		today.minimumTemperature = 2;
-		today.dress = "우산, 패딩";
-		
-		tomorrow.weather = "눈";
-		tomorrow.highestTemperature = 3;
-		tomorrow.minimumTemperature = -3;
-		tomorrow.dress = "우산, 패딩";
+		today.insertWeather("비", 3, -3, "우산, 패딩");
+		tomorrow.insertWeather("눈", 3, -6, "우산, 패딩");
 		
 		/** 값 출력 */
 		System.out.println("오늘");
-		System.out.println("날씨: " + today.weather);
-		System.out.println("최고 기온: " + today.highestTemperature);
-		System.out.println("최저 기온: " + today.minimumTemperature);
-		System.out.println("옷차림: " + today.dress);
-		System.out.println("----------");
+		today.printWeather();
 		
 		System.out.println("내일");
-		System.out.println("날씨: " + tomorrow.weather);
-		System.out.println("최고 기온: " + tomorrow.highestTemperature);
-		System.out.println("최저 기온: " + tomorrow.minimumTemperature);
-		System.out.println("옷차림: " + tomorrow.dress);
-		System.out.println("----------");
+		tomorrow.printWeather();
 	}
 }
 
--- src/personal/project/main/vo/WeatherVo.java
+++ src/personal/project/main/vo/WeatherVo.java
@@ -10,4 +10,21 @@ public class WeatherVo {
 	public int highestTemperature;
 	public int minimumTemperature;
 	public String dress;
+	
+	/** 날씨 속성 저장 함수 */
+	public void insertWeather(String weather, int highestTemperature, int minimumTemperature, String dress) {
+		this.weather = weather;
+		this.highestTemperature = highestTemperature;
+		this.minimumTemperature = minimumTemperature;
+		this.dress = dress;
+	}
+	
+	/** 날씨 속성 출력 함수 */
+	public void printWeather() {
+		System.out.println("날씨: " + this.weather);
+		System.out.println("최고 기온: " + this.highestTemperature);
+		System.out.println("최저 기온: " + this.minimumTemperature);
+		System.out.println("옷차림: " + this.dress);
+		System.out.println("----------");
+	}
 }
